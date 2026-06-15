b9a26344b09a448a98039bed3af6168f4591771c
Dwi Sekar <sekarandini78@gmail.com>
2026-06-15 15:30:03 +0700

    tambah src & koneksi java

---
+++ src/DbKoneksi.java
@@ -0,0 +1,27 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbKoneksi {
+    private static final String URL = "jdbc:mysql://localhost:3306/db_chronosys?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASS = "";
+
+    public static Connection getKoneksi() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(URL, USER, PASS);
+            System.out.println("Koneksi ke Database BERHASIL!");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver tidak ditemukan: " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Gagal koneksi: " + e.getMessage());
+        }
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        getKoneksi();
+    }
+}
