e328f8b9b942b45db820567cd25c6480da7543a5
Dwi Sekar <sekarandini78@gmail.com>
2026-06-19 12:54:47 +0700

    tambah beberapa desain + jcalender

--- src/Kegiatan.java
+++ src/Kegiatan.java
@@ -7,38 +7,100 @@
  *
  * @author Dwi Sekar
  */
+/**
+ *
+ * @author Dwi Sekar
+ */
 public class Kegiatan {
     private int idKegiatan;
+    private int idPengguna;
     private String namaKegiatan;
+    private String kategori;
     private String tanggal;
-    private String waktu;
-    private String tempat;
+    private String jam;
+    private String prioritas;
+    private String keterangan;
     private String status;
 
-    public Kegiatan(int idKegiatan, String namaKegiatan, String tanggal, String waktu, String tempat, String status) {
+    public Kegiatan(int idKegiatan, int idPengguna, String namaKegiatan, String kategori, String tanggal, String jam, String prioritas, String keterangan, String status) {
         this.idKegiatan = idKegiatan;
+        this.idPengguna = idPengguna;
         this.namaKegiatan = namaKegiatan;
+        this.kategori = kategori;
         this.tanggal = tanggal;
-        this.waktu = waktu;
-        this.tempat = tempat;
+        this.jam = jam;
+        this.prioritas = prioritas;
+        this.keterangan = keterangan;
+        this.status = status;
+    }
+    public Kegiatan() {
+    }
+    
+    public int getIdKegiatan() {
+        return idKegiatan;
+    }
+    public void setIdKegiatan(int idKegiatan) {
+        this.idKegiatan = idKegiatan;
+    }
+
+    public int getIdPengguna() {
+        return idPengguna;
+    }
+    public void setIdPengguna(int idPengguna) {
+        this.idPengguna = idPengguna;
+    }
+
+    public String getNamaKegiatan() {
+        return namaKegiatan;
+    }
+    public void setNamaKegiatan(String namaKegiatan) {
+        this.namaKegiatan = namaKegiatan;
+    }
+
+    public String getKategori() {
+        return kategori;
+    }
+    public void setKategori(String kategori) {
+        this.kategori = kategori;
+    }
+
+    public String getTanggal() {
+        return tanggal;
+    }
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
+
+    public String getJam() {
+        return jam;
+    }
+    public void setJam(String jam) {
+        this.jam = jam;
+    }
+
+    public String getPrioritas() {
+        return prioritas;
+    }
+    public void setPrioritas(String prioritas) {
+        this.prioritas = prioritas;
+    }
+
+    public String getKeterangan() {
+        return keterangan;
+    }
+    public void setKeterangan(String keterangan) {
+        this.keterangan = keterangan;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
         this.status = status;
     }
 
-    public int getIdKegiatan() { return idKegiatan; }
-    public void setIdKegiatan(int idKegiatan) { this.idKegiatan = idKegiatan; }
-    public String getNamaKegiatan() { return namaKegiatan; }
-    public void setNamaKegiatan(String namaKegiatan) { this.namaKegiatan = namaKegiatan; }
-    public String getTanggal() { return tanggal; }
-    public void setTanggal(String tanggal) { this.tanggal = tanggal; }
-    public String getWaktu() { return waktu; }
-    public void setWaktu(String waktu) { this.waktu = waktu; }
-    public String getTempat() { return tempat; }
-    public void setTempat(String tempat) { this.tempat = tempat; }
-    public String getStatus() { return status; }
-    public void setStatus(String status) { this.status = status; }
-
     @Override
     public String toString() {
-        return idKegiatan + " | " + namaKegiatan + " | " + tanggal + " | " + waktu + " | " + tempat + " | " + status;
+        return idKegiatan + " | " + namaKegiatan + " | " + tanggal + " " + jam + " | " + status;
     }
 }
